59b09141b6cfcf6aa9fcf0d0f973384193dbca8c
Rangga Prangwedana <vergil1st@gmail.com>
2025-10-21 19:14:36 +0700

    Exercise for day 1

---
+++ day-01/exercise.js
@@ -0,0 +1,37 @@
+// Comments can make code readable
+// Welcome to 30DaysofJavascript
+/*
+Comments can make code readable,
+easy to reuse and informative.
+*/
+// variable.js
+let dragon = "Deathwing";
+let death = true;
+let megadeath;
+let numbers = 24;
+let empty = null;
+
+// typeof.js
+console.log(typeof dragon);
+console.log(typeof death);
+console.log(typeof numbers);
+console.log(typeof empty);
+
+let firstName = "Rangga";
+let lastName = "Januwarsa";
+let whats = "married";
+let country = "Indonesia";
+
+console.log(firstName, " ", lastName, " ", whats, " ", country);
+
+let first = "Rangga",
+  last = "Januwarsa",
+  marriage = "married",
+  domicile = "Indonesia";
+
+console.log(first, " ", last, " ", marriage, " ", domicile);
+
+let myAge = 34;
+let yourAge = 28;
+console.log("I am ", myAge, " years old.");
+console.log("You are ", yourAge, " years old.");
